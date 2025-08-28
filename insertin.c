a75cc3d412ffc2ce37e1ce0239874b9bfa90da82
MK yadav <manikantayadav243@gmail.com>
2025-08-28 11:47:10 +0530

    Add files via upload

---
+++ insertin.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void insert();
+voiddl();
+void search();
+void display();
+struct ode
+{\
+int data
+srtuct node*next;
+}
+struct node*n,head=null,*temp,*p=null
+void main()
+{int ch;
+	while(1)
+{prntf("enter your choice")
+printf("1.insert 2.del 3.search 4.display 5.exit";);
+scanf("%d",&ch);
+switch(ch)
+{
+case 1;insert();
+	   break();
+case 2;del();
+       BREAK();
+case 3;search();
+	   break();
+case 4;display();
+	   break();
+case 5;exit();
+}
+}
+}
+void insert();
+{
+	int c,t;
+	n=(struct node*)malloc(size of (struct node));
+	printf("enter data");
+	scanf("%d",&n->data);
+	if(head==null)
+	{
+		head=n;
+		n->next=null;
+		}
+	else
+	{
+	printf("1.begin 2.ending 3.after some node 4.before some node ");
+	printf("enter your choice");
+	scanf("%d",&c)
+	switch(c)
+	}
+}
